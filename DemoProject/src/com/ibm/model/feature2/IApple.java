e73e37a40ace706cd2d780b3eb676543a2625b78
sm <sureshkumarmadhra@yahoo.com>
2018-03-01 14:57:26 +0530

    Feature2 Team added one method to IApple Class

--- DemoProject/src/com/ibm/model/feature2/IApple.java
+++ DemoProject/src/com/ibm/model/feature2/IApple.java
@@ -2,4 +2,9 @@ package com.ibm.model.feature2;
 
 public class IApple {
 
+		// Volume Method added by Feature2 Team
+		public void volume() {
+			System.out.println("This is my method to caculcate Valume");
+		}
+	
 }
