cfcf994cfba47d55ba0a206a20310dafeaedd349
sm <sureshkumarmadhra@yahoo.com>
2018-03-01 15:38:33 +0530

    Payment.java file updated by Feature1 Team

--- DemoProject/src/com/ibm/model/bs/Payment.java
+++ DemoProject/src/com/ibm/model/bs/Payment.java
@@ -1,5 +1,15 @@
 package com.ibm.model.bs;
 
 public class Payment {
-
+	public String name = "SURESH";
+	public String address = "DELHI";
+	public String message = "Hello";
+	
+	
+	public String name1 = "SURESH";
+	public String address1 = "DELHI";
+	public String message1 = "Hello";
+	
+	
+	
 }
