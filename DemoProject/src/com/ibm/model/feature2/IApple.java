b5333e034a387377a077a3c267f8956adcdb1758
sm <sureshkumarmadhra@yahoo.com>
2018-03-01 14:54:36 +0530

    Feature1 Team added changes to IApple class

--- DemoProject/src/com/ibm/model/feature2/IApple.java
+++ DemoProject/src/com/ibm/model/feature2/IApple.java
@@ -2,4 +2,14 @@ package com.ibm.model.feature2;
 
 public class IApple {
 
+	//* IApple Constructor added by Feature1 Team
+	public IApple() {
+			
+		}
+		
+	//* main method added by Feature1 Team
+		
+		public void main() {
+			
+		}
 }
